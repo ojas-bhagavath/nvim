8a9084a920af1b09d8c3a4ffb700fa136525f028
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-10-01 14:25:10 +0530

    Changed flash jump keymap

--- lua/core/plugins/flash.lua
+++ lua/core/plugins/flash.lua
@@ -4,20 +4,12 @@ return {
     opts = {},
     keys = {
         {
-            "s",
+            "!",
             mode = { "n", "x", "o" },
             function()
                 require("flash").jump()
             end,
             desc = "Flash",
-        },
-        {
-            "S",
-            mode = { "n", "x", "o" },
-            function()
-                require("flash").treesitter()
-            end,
-            desc = "Flash Treesitter",
         },
     },
 }
