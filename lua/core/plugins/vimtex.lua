348177d232c82493f3f743a5b37e1fc6f467e934
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-10-05 10:52:08 +0530

    Housekeeping

--- lua/core/plugins/vimtex.lua
+++ lua/core/plugins/vimtex.lua
@@ -1,7 +1,6 @@
 return {
     "lervag/vimtex",
     config = function()
-        -- vim.g.vimtex_mappings_disable = { ["n"] = { "K" } }
         vim.g.vimtex_quickfix_open_on_warning = 0
         vim.g.vimtex_view_method = "sioyek"
         vim.cmd([[autocmd BufNewFile,BufRead *.md so $VIMRUNTIME/syntax/tex.vim]])
