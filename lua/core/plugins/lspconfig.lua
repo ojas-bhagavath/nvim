ffb0804c47ac044afe6c4826628857884d247bad
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-10-16 16:45:58 +0530

    Update lua lspconfig to not show missing global variable vim warning

--- lua/core/plugins/lspconfig.lua
+++ lua/core/plugins/lspconfig.lua
@@ -174,6 +174,9 @@ return { -- LSP Configuration & Plugins
                         completion = {
                             callSnippet = "Replace",
                         },
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
                         -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
                         -- diagnostics = { disable = { 'missing-fields' } },
                     },
