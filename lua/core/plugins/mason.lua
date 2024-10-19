3cd070bf137a20b503619c387e789fcc46d3f179
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-10-19 20:46:15 +0530

    Update mason config

---
+++ lua/core/plugins/mason.lua
@@ -0,0 +1,45 @@
+return {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = {
+        {
+            "<leader>cm",
+            "<cmd>Mason<cr>",
+            desc = "Mason",
+        },
+    },
+    build = ":MasonUpdate",
+    opts_extend = { "ensure_installed" },
+    opts = {
+        ui = {
+            icons = {
+                package_installed = "",
+                package_pending = "",
+                package_uninstalled = "",
+            },
+            border = "rounded",
+        },
+        ensure_installed = {
+            "css-lsp",
+            "html-lsp",
+            "lua-language-server",
+            "marksman",
+            "shellcheck",
+            "stylua",
+            "taplo",
+            "texlab",
+        },
+    },
+    config = function(_, opts)
+        require("mason").setup(opts)
+        local mr = require("mason-registry")
+        mr.refresh(function()
+            for _, tool in ipairs(opts.ensure_installed) do
+                local p = mr.get_package(tool)
+                if not p:is_installed() then
+                    p:install()
+                end
+            end
+        end)
+    end,
+}
