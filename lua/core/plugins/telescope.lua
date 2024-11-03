907eb62b501e1a298285b0c353602ffa6b76b175
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-11-04 02:06:35 +0530

    Add multiopen function to telescope

--- lua/core/plugins/telescope.lua
+++ lua/core/plugins/telescope.lua
@@ -27,15 +27,32 @@ return {
         },
     },
     config = function()
+        local multiopen = function(prompt_bufnr)
+            local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+            local multi = picker:get_multi_selection()
+            if not vim.tbl_isempty(multi) then
+                require("telescope.actions").close(prompt_bufnr)
+                for _, j in pairs(multi) do
+                    if j.path ~= nil then
+                        vim.cmd(string.format("%s %s", "edit", j.path))
+                    end
+                end
+            else
+                require("telescope.actions").select_default(prompt_bufnr)
+            end
+        end
         require("telescope").setup({
             defaults = {
                 mappings = {
                     i = {
                         ["<C-k>"] = require("telescope.actions").move_selection_previous,
                         ["<C-j>"] = require("telescope.actions").move_selection_next,
+                        ["<CR>"] = multiopen,
                     },
                     n = {
                         ["d"] = require("telescope.actions").delete_buffer,
+                        ["q"] = require("telescope.actions").close,
+                        ["<CR>"] = multiopen,
                     },
                 },
             },
