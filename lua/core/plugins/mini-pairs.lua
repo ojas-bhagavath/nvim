b2311b09d55994e4c67d60041c1e0c70366d058f
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-11-30 10:02:23 +0530

    Replace nvim-autopairs with mini.pairs

---
+++ lua/core/plugins/mini-pairs.lua
@@ -0,0 +1,13 @@
+return {
+    "echasnovski/mini.pairs",
+    event = { "InsertEnter" },
+    main = "mini.pairs",
+    opts = {},
+    init = function()
+        -- Create symmetrical `$$` pair only in Tex files
+        local map_tex = function()
+            MiniPairs.map_buf(0, "i", "$", { action = "closeopen", pair = "$$" })
+        end
+        vim.api.nvim_create_autocmd("FileType", { pattern = "tex", callback = map_tex })
+    end,
+}
