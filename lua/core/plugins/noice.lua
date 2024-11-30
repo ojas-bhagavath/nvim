ce272e2d9829153ec6aab471813379dcb1467e10
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-11-30 21:59:19 +0530

    Housekeeping

--- lua/core/plugins/noice.lua
+++ lua/core/plugins/noice.lua
@@ -19,11 +19,6 @@ return {
             progress = {
                 enabled = false,
             },
-            override = {
-                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                ["vim.lsp.util.stylize_markdown"] = true,
-                ["cmp.entry.get_documentation"] = true,
-            },
         },
         presets = {
             command_palette = true,
