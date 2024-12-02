d3ee7b23cb21201777de4f5e70dab101c6a0c03a
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-12-03 03:23:45 +0530

    Minor improvement

--- lua/core/plugins/mini-pairs.lua
+++ lua/core/plugins/mini-pairs.lua
@@ -8,6 +8,6 @@ return {
         local map_tex = function()
             MiniPairs.map_buf(0, "i", "$", { action = "closeopen", pair = "$$" })
         end
-        vim.api.nvim_create_autocmd("FileType", { pattern = "tex", callback = map_tex })
+        vim.api.nvim_create_autocmd("FileType", { pattern = { "tex", "markdown" }, callback = map_tex })
     end,
 }
