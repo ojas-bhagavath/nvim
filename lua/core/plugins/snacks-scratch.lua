611a5b3dd98f412774f0a87d65d3dac55ae837d9
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-12-03 03:23:32 +0530

    Add scratch module to snacks.

---
+++ lua/core/plugins/snacks-scratch.lua
@@ -0,0 +1,24 @@
+return {
+    "folke/snacks.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        scratch = {
+            ft = "markdown",
+            win = {
+                width = 0.8,
+                height = 0.8,
+                border = "rounded",
+            },
+        },
+    },
+    keys = {
+        {
+            "<leader>.",
+            function()
+                Snacks.scratch()
+            end,
+            desc = "Toggle Scratch Buffer",
+        },
+    },
+}
