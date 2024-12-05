20e8afe89eca86d69abcd727b9d26f17d34633a1
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-12-06 01:01:29 +0530

    Update arrow.lua

--- lua/core/snippets/tex/arrows.lua
+++ lua/core/snippets/tex/arrows.lua
@@ -1,9 +1,9 @@
 return {
-    s({ trig = "->", snippetType = "autosnippet" }, t("\\rightarrow"), { condition = in_mathzone }),
-    s({ trig = "-<", snippetType = "autosnippet" }, t("\\leftarrow"), { condition = in_mathzone }),
-    s({ trig = "mapsto", snippetType = "autosnippet" }, t("\\mapsto"), { condition = in_mathzone }),
-    s({ trig = "inclusion", snippetType = "autosnippet" }, t("\\hookrightarrow"), { condition = in_mathzone }),
-    s({ trig = "=>", snippetType = "autosnippet" }, t("\\implies"), { condition = in_mathzone }),
-    s({ trig = "=<", snippetType = "autosnippet" }, t("\\impliedby"), { condition = in_mathzone }),
+    s({ trig = "-.", snippetType = "autosnippet" }, t("\\to"), { condition = in_mathzone }),
+    s({ trig = "-,", snippetType = "autosnippet" }, t("\\leftarrow"), { condition = in_mathzone }),
+    s({ trig = "maps", snippetType = "autosnippet" }, t("\\mapsto"), { condition = in_mathzone }),
+    s({ trig = "incl", snippetType = "autosnippet" }, t("\\hookrightarrow"), { condition = in_mathzone }),
+    s({ trig = "=.", snippetType = "autosnippet" }, t("\\implies"), { condition = in_mathzone }),
+    s({ trig = "=,", snippetType = "autosnippet" }, t("\\impliedby"), { condition = in_mathzone }),
     s({ trig = "iff", snippetType = "autosnippet" }, t("\\iff"), { condition = in_mathzone }),
 }
