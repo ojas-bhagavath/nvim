aa60f0dd69b1358e6bba715b9a1ced2ceee63037
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-12-25 22:39:17 +0530

    Update lualine config

--- lua/core/plugins/lualine.lua
+++ lua/core/plugins/lualine.lua
@@ -1,133 +1,123 @@
 return {
     "nvim-lualine/lualine.nvim",
     event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-    opts = function()
-        local function getbufcount()
-            local num_bufs = 0
-            for i = 1, vim.api.nvim_buf_count() do
-                if vim.api.nvim_buf_is_loaded(i) then
-                    num_bufs = num_bufs + 1
-                end
-            end
-        end
-        return {
-            options = {
-                icons_enabled = true,
-                globalstatus = true,
-                disabled_filetypes = {
-                    statusline = {
-                        "snacks_dashboard",
-                        "snacks_terminal",
-                        "dashboard",
-                        "yazi",
-                        "mason",
-                    },
-                },
-                component_separators = "",
-                section_separators = {
-                    left = "",
-                    right = "",
+    opts = {
+        options = {
+            icons_enabled = true,
+            globalstatus = true,
+            disabled_filetypes = {
+                statusline = {
+                    "snacks_dashboard",
+                    "snacks_terminal",
+                    "dashboard",
+                    "yazi",
+                    "mason",
                 },
             },
-            sections = {
-                lualine_a = {
-                    {
-                        "mode",
-                        separator = {
-                            right = "",
-                            left = "",
-                        },
+            component_separators = "",
+            section_separators = {
+                left = "",
+                right = "",
+            },
+        },
+        sections = {
+            lualine_a = {
+                {
+                    "mode",
+                    separator = {
+                        right = "",
+                        left = "",
                     },
                 },
-                lualine_b = {
-                    {
-                        "diff",
-                        symbols = {
-                            added = " ",
-                            modified = " ",
-                            removed = " ",
-                        },
-                        separator = {
-                            left = "",
-                            right = "",
-                        },
-                        on_click = function()
-                            Snacks.lazygit.open()
-                        end,
+            },
+            lualine_b = {
+                {
+                    "diff",
+                    symbols = {
+                        added = " ",
+                        modified = " ",
+                        removed = " ",
                     },
-                    {
-                        "diagnostics",
-                        symbols = {
-                            error = " ",
-                            warn = " ",
-                            hint = " ",
-                            info = " ",
-                        },
-                        separator = {
-                            left = "",
-                            right = "",
-                        },
-                        on_click = function()
-                            vim.cmd("LspInfo")
-                        end,
+                    separator = {
+                        left = "",
+                        right = "",
+                    },
+                    on_click = function()
+                        Snacks.lazygit.open()
+                    end,
+                },
+                {
+                    "diagnostics",
+                    symbols = {
+                        error = " ",
+                        warn = " ",
+                        hint = " ",
+                        info = " ",
+                    },
+                    separator = {
+                        left = "",
+                        right = "",
+                    },
+                    on_click = function()
+                        vim.cmd("LspInfo")
+                    end,
+                },
+            },
+            lualine_c = {
+                {
+                    "filename",
+                    file_status = true,
+                    newfile_status = true,
+                    path = 3,
+                    shorting_target = 40,
+                    symbols = {
+                        modified = "●",
+                        readonly = "",
+                        unnamed = "[No Name]",
+                        newfile = "[New]",
                     },
                 },
-                lualine_c = {
-                    {
-                        "filename",
-                        file_status = true,
-                        newfile_status = true,
-                        path = 3,
-                        shorting_target = 40,
-                        symbols = {
-                            modified = "●",
-                            readonly = "",
-                            unnamed = "[No Name]",
-                            newfile = "[New]",
-                        },
-                    },
+            },
+            lualine_x = {
+                {
+                    function()
+                        local starts = vim.fn.line("v")
+                        local ends = vim.fn.line(".")
+                        local count = starts <= ends and ends - starts + 1 or starts - ends + 1
+                        local wc = vim.fn.wordcount()
+                        return count .. ":" .. wc["visual_chars"]
+                    end,
+                    cond = function()
+                        return vim.fn.mode():find("[Vv]") ~= nil
+                    end,
                 },
-                lualine_x = {
-                    {
-                        function()
-                            local starts = vim.fn.line("v")
-                            local ends = vim.fn.line(".")
-                            local count = starts <= ends and ends - starts + 1 or starts - ends + 1
-                            local wc = vim.fn.wordcount()
-                            return count .. ":" .. wc["visual_chars"]
-                        end,
-                        cond = function()
-                            return vim.fn.mode():find("[Vv]") ~= nil
-                        end,
-                    },
-                    {
-                        "searchcount",
-                    },
+                {
+                    "searchcount",
                 },
+            },
 
-                lualine_y = {
-                    {
-                        "filetype",
-                        separator = {
-                            left = "",
-                            right = "",
-                        },
-                    },
-                },
-                lualine_z = {
-                    {
-                        "location",
-                        padding = {
-                            left = 0,
-                            right = 0,
-                        },
-                        separator = {
-                            left = "",
-                            right = "",
-                        },
+            lualine_y = {
+                {
+                    "filetype",
+                    separator = {
+                        left = "",
+                        right = "",
                     },
                 },
             },
-        }
-    end,
+            lualine_z = {
+                {
+                    "location",
+                    padding = {
+                        left = 0,
+                        right = 0,
+                    },
+                    separator = {
+                        left = "",
+                        right = "",
+                    },
+                },
+            },
+        },
+    },
 }
