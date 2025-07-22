4cd32a13737457c1de47b2335d5bc3e8f5ff78e6
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2025-07-22 21:35:15 +0530

    feat(lint.lua): remove commented out code to improve readability and reduce clutter fix(lint.lua): fix autocmd creation by specifying events in a table instead of comma-separated string

--- lua/core/plugins/lint.lua
+++ lua/core/plugins/lint.lua
@@ -5,8 +5,6 @@ return {
         require("lint").linters_by_ft = {
             ["shell"] = { "shellcheck" },
         }
-        -- Create autocommand which carries out the actual linting
-        -- on the specified events.
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
         vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
             group = lint_augroup,
