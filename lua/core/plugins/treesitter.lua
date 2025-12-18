38ea6cd86e055c0c3beaeee5a7a3ac32039d25e4
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2025-12-17 18:31:13 -0600

    fix(treesitter.lua): remove redundant curly braces around plugin configuration

--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -1,58 +1,57 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-        dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    dependencies = {
+        {
             "windwp/nvim-ts-autotag",
             opts = {},
         },
-        build = ":TSUpdate",
-        opts = {
-            ensure_installed = {
-                "bash",
-                "beancount",
-                "css",
-                "gitignore",
-                "html",
-                "javascript",
-                "json5",
-                "ledger",
-                "lua",
-                "luadoc",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "regex",
-                "scss",
-                "toml",
-                "vim",
-                "vimdoc",
-                "toml",
-                "yaml",
-            },
-            ignore_install = { "latex" },
-            autoinstall = true,
-            highlight = {
-                enable = true,
-                disable = { "latex" },
-            },
-            indent = {
-                enable = true,
-                disable = { "latex" },
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-Space>",
-                    node_incremental = "<C-Space>",
-                    scope_incremental = false,
-                    node_decremental = "<C-S-Space>",
-                },
+    },
+    build = ":TSUpdate",
+    opts = {
+        ensure_installed = {
+            "bash",
+            "beancount",
+            "css",
+            "gitignore",
+            "html",
+            "javascript",
+            "json5",
+            "ledger",
+            "lua",
+            "luadoc",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "regex",
+            "scss",
+            "toml",
+            "vim",
+            "vimdoc",
+            "yaml",
+        },
+        ignore_install = { "latex" },
+        auto_install = true,
+        highlight = {
+            enable = true,
+            disable = { "latex" },
+        },
+        indent = {
+            enable = true,
+            disable = { "latex" },
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<C-Space>",
+                node_incremental = "<C-Space>",
+                scope_incremental = false,
+                node_decremental = "<C-S-Space>",
             },
         },
-        config = function(_, opts)
-            require("nvim-treesitter.install").prefer_git = true
-            require("nvim-treesitter.configs").setup(opts)
-        end,
     },
+    config = function(_, opts)
+        require("nvim-treesitter.install").prefer_git = true
+        require("nvim-treesitter.configs").setup(opts)
+    end,
 }
