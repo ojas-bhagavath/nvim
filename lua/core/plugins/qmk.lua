612212b65fec74bba639bafe8b7f6962d63c888a
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2026-04-21 19:14:04 -0500

    add qmk.nvim plugin to locally edit zmk keymap file

---
+++ lua/core/plugins/qmk.lua
@@ -0,0 +1,17 @@
+return {
+    "codethread/qmk.nvim",
+    ft = "dts",
+    enabled = true,
+    event = "VeryLazy",
+    opts = {
+        name = "sofle",
+        variant = "zmk",
+        layout = {
+            "x x x x x x _ _ _ _ x x x x x x",
+            "x x x x x x _ _ _ _ x x x x x x",
+            "x x x x x x _ _ _ _ x x x x x x",
+            "x x x x x x x _ _ x x x x x x x",
+            "_ _ x x x x x _ _ x x x x x _ _",
+        },
+    },
+}
