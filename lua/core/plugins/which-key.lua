ca815eff751211b136e10f8fe02af86bf01d0936
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2026-09-08 13:06:27 -0500

    20260908-13:06

--- lua/core/plugins/which-key.lua
+++ lua/core/plugins/which-key.lua
@@ -1,7 +1,31 @@
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+        preset = "helix",
+        spec = {
+            {
+                mode = { "n", "x" },
+                { "<leader><tab>", group = "tab", icon = "󰓩" },
+                { "<leader>?", group = "which-key", icon = "" },
+                { "<leader>\\", icon = "" },
+                { "<leader>b", group = "buffer", icon = "" },
+                { "<leader>c", group = "code", icon = "" },
+                { "<leader>d", group = "diagnostics", icon = "" },
+                { "<leader>f", group = "file", icon = "" },
+                { "<leader>g", group = "git", icon = "" },
+                { "<leader>l", group = "lsp", icon = "" },
+                { "<leader>o", group = "toggle", icon = "" },
+                { "<leader>q", group = "sessions", icon = "" },
+                { "<leader>r", group = "reload", icon = "󰑓" },
+                { "<leader>s", group = "split", icon = "" },
+                { "<leader>t", group = "terminal", icon = "" },
+                { "<leader>u", group = "ui" },
+                { "<leader>x", group = "execute", icon = "" },
+                { "<localleader>l", group = "vimtex", icon = "" },
+            },
+        },
+    },
     keys = {
         {
             "<leader>?",
@@ -11,25 +35,4 @@ return {
             desc = "Buffer Local Keymaps (which-key)",
         },
     },
-    config = function()
-        require("which-key").add({
-            { "<leader><tab>", group = "tab", icon = "󰓩" },
-            { "<leader>?", group = "which-key", icon = "" },
-            { "<leader>\\", icon = "" },
-            { "<leader>b", group = "buffer", icon = "" },
-            { "<leader>c", group = "code", icon = "" },
-            { "<leader>d", group = "diagnostics", icon = "" },
-            { "<leader>f", group = "file", icon = "" },
-            { "<leader>g", group = "git", icon = "" },
-            { "<leader>l", group = "lsp", icon = "" },
-            { "<leader>o", group = "toggle", icon = "" },
-            { "<leader>q", group = "sessions", icon = "" },
-            { "<leader>r", group = "reload", icon = "󰑓" },
-            { "<leader>s", group = "split", icon = "" },
-            { "<leader>t", group = "terminal", icon = "" },
-            { "<leader>u", group = "ui" },
-            { "<leader>x", group = "execute", icon = "" },
-            { "<localleader>l", group = "vimtex", icon = "" },
-        })
-    end,
 }
